b187ee60ff6908f71ee1c256f1c1345411cf128c
Olger Castro Palacios <olger60019castro@gmail.com>
2021-11-06 09:46:00 -0500

    06112021 v02

--- app/model/MSdb.php
+++ app/model/MSdb.php
@@ -4,9 +4,9 @@ class ConexionConsulta
     static public function conectar()
     {
         $link = new PDO(
-            'odbc: Driver={SQL Server};Server=172.16.0.21;Database=SIGH',
-            'consulta',
-            'consulta'
+            'sqlsrv:Server=KILLASISA;Database=SIGH',
+            'sa',
+            '123456'
             // 'sqlsrv:Server=VISION;Database=SIGH',
             // 'sa',
             // 'Sistemas2021+-+'
