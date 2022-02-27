02b91e06fff2821ee0571244b7a671d505f39834
Olger Castro <olger60019castro@gmail.com>
2022-02-26 23:42:27 -0500

    2808

--- app/model/MSdb.php
+++ app/model/MSdb.php
@@ -4,12 +4,13 @@ class ConexionConsulta
     static public function conectar()
     {
         $link = new PDO(
-            'sqlsrv:Server=192.168.32.123;Database=SIGH',
-            'sa',
-            'Heves.2016'
-            // 'sqlsrv:Server=KILLASISA;Database=SIGH',
+            // 'sqlsrv:Server=192.168.32.123;Database=SIGH',
             // 'sa',
-            // '123456'
+            // 'Heves.2016'
+            'sqlsrv:Server=KILLASISA;Database=SIGH',
+            'sa',
+            '123456'
+            
         );
         // $link->exec("set names utf8");
         return $link;
